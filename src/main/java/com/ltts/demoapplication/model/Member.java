3b07d1435c37df22d1f574625fd2c48244e2b764
Akash0299 <akash0299@gmail.com>
2022-04-04 10:59:25 +0530

    Update Member.java

--- src/main/java/com/ltts/demoapplication/model/Member.java
+++ src/main/java/com/ltts/demoapplication/model/Member.java
@@ -14,37 +14,47 @@ public class Member {
 	public Member() {
 		super();
 	}
-	public Member(String uname, String pswd, String email, int phone) {
-		super();
-		this.username = uname;
-		this.password = pswd;
-		this.email = email;
-		this.phone = phone;
-	}
-	public String getUname() {
+	
+	public String getUsername() {
 		return username;
 	}
-	public void setUname(String uname) {
-		this.username = uname;
+
+	public void setUsername(String username) {
+		this.username = username;
 	}
-	public String getPswd() {
+
+	public String getPassword() {
 		return password;
 	}
-	public void setPswd(String pswd) {
-		this.password = pswd;
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public int getPhone() {
 		return phone;
 	}
+
 	public void setPhone(int phone) {
 		this.phone = phone;
 	}
+
+	public Member(String username, String password, String email, int phone) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.phone = phone;
+	}
+
 	@Override
 	public String toString() {
 		return "Member username=" + username + ", pasword=" + password + ", email=" + email + ", phone=" + phone;
