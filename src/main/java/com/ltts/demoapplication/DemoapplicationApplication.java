5a03332fbf9390871b0d6be81673682d164aa226
Akash0299 <akash0299@gmail.com>
2022-04-04 10:59:18 +0530

    Update DemoapplicationApplication.java

--- src/main/java/com/ltts/demoapplication/DemoapplicationApplication.java
+++ src/main/java/com/ltts/demoapplication/DemoapplicationApplication.java
@@ -2,10 +2,8 @@ package com.ltts.demoapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class DemoapplicationApplication {
 
 	public static void main(String[] args) {
